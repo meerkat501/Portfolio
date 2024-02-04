d046aeb3e8d31b1f6ecfd08744ac292cb8c324c4
meerkat501 <leonardosoto911@gmail.com>
2024-02-03 20:34:39 -0600

    added contact and resume.jsx

---
+++ Contact.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Contact = () => {
+    return (
+        <section>
+            <h2>Contact</h2>
+            <p> How to contact me</p>
+        </section>
+    );
+};
+
+export default Contact;
---
+++ Resume.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Resume = () => {
+    return (
+        <section>
+            <h2>Resume</h2>
+            <p> How to contact me</p>
+        </section>
+    );
+};
+
+export default Resume;
