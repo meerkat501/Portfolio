be14220f7c62e1648cd3112ba7934e1bbe50c3a6
meerkat501 <leonardosoto911@gmail.com>
2024-02-03 19:48:24 -0600

    added about.jsx

---
+++ About.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <section>
+            <h2>About Me</h2>
+            <img src="" alt="Developer" />
+            <p>BIO WILL GO HERE</p>
+        </section>
+    );
+};
+
+export default About;
