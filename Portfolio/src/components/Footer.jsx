158978b24a66f2489ef1c9555eb7fe29f76c5e59
meerkat501 <leonardosoto911@gmail.com>
2024-02-06 18:28:54 -0600

    added background image and added a summary about me

--- Portfolio/src/components/Footer.jsx
+++ Portfolio/src/components/Footer.jsx
@@ -1,11 +1,10 @@
 const Footer = () => {
     return (
         <footer>
-            <a href="https://github.com/meerkat501"> GitHub</a>
-            <a href="https://linkedin.com"> LinkedIn </a>
-            <a href="https://twitter.com"> Twitter </a>
+            <a href="https://github.com/meerkat501"> GitHub </a>
+            <a href="https://www.linkedin.com/in/leonardo-soto-942b17239?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BclEqQr1%2FRFSxUeV1Y2XeUQ%3D%3D"> LinkedIn </a>
         </footer>
     )
 };
 
-export default Footer;
+export default Footer;0
